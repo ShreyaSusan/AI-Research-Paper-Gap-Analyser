60c93e107a932032595e583626db650aea510e6d
Shreya Susan Sajan <shreyasajan5002@gmail.com>
2026-05-25 14:34:33 +0530

    Added Docker deployment configuration

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/researchdb"
+DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/researchdb"
 
 engine = create_engine(DATABASE_URL)
 
